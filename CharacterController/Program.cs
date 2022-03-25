c0b13ad45be85bc1b7da93ae0de71e9dcf241b08
Caleb Allen <caleb@caleballen.com>
2022-03-24 21:56:57 -0700

    Adding a test character to get a visual on things.

--- CharacterController/Program.cs
+++ CharacterController/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CharacterLib;
 
 namespace CharacterController
 {
@@ -14,11 +17,55 @@ namespace CharacterController
                 Console.Write("Enter comm >");
                 string cmd = Console.ReadLine();
 
-                Console.Write($"You entered '{cmd}'");
+                var billy = getTestCharacter();
+
+                Console.Write($"You entered '{billy.Stats}'");
 
                 done = true;
             }
-            
+
+        }
+
+        private static Character getTestCharacter()
+        {
+            return new Character()
+            {
+                Name = "Billy Boghollow",
+                HitPoints = 100,
+                Stats = new VitalStatistics
+                {
+                    Strength = new Stat { Score = 12 },
+                    Dexterity = new Stat { Score = 12 },
+                    Constitution = new Stat { Score = 12 },
+                    Intelligence = new Stat { Score = 12 },
+                    Wisdom = new Stat { Score = 12 },
+                    Charisma = new Stat { Score = 12 }
+                },
+                CharacterClass = new CharacterClass()
+                {
+                    Name = "Fighter",
+                    HitDie = 10,
+                    Features = new List<Feature>() { new Feature() {
+                        Name = "Second Wind",
+                        Level = 1,
+                        Rules = "Some text\n\n● A Markdown Bullet\n\n● Another Bullet",
+                        Options = new List<Option>()
+                        {
+                            new Option()
+                            {
+                                Name = "Heal",
+                                Action = "Bonus Action",
+                                Effect = new GameEffect() {
+                                    Target = "Self",
+                                    Formula = "5",
+                                    EffectType = Effect.Heal
+                                }
+                            }
+                        }
+                    } },
+                    Proficiencies = new List<string>() { "Medium Armor", "Light Armor", "Simple Weapons" }
+                }
+            };
         }
     }
 }
